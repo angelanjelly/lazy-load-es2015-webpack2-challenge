c086b4e6455de1499ffdcfee4588847382252df6
Angela M. Choi <angela.minchoi@gmail.com>
2016-12-10 13:14:24 -0800

    lazy loading by using webpack's require.ensure

--- src/main.js
+++ src/main.js
@@ -1,16 +1,18 @@
-// Importing from Animations and Messaging
-import { MerryXmas } from './animations';
-import { Alert } from './messaging'
-
 // Listener to fire up Merry Xmas
 document.getElementById( 'button' ).addEventListener( 'click', e => {
-  if (!this.merryXmas) {
-    this.merryXmas = new MerryXmas( 'effect' );
-  }
-  this.merryXmas.animate();
+	require.ensure([], function (require) {
+		var MerryXmas = require('./animations').MerryXmas;
+		if (!this.merryXmas) {
+			this.merryXmas = new MerryXmas( 'effect' );
+		}
+		this.merryXmas.animate();
+	} );
 } );
 
 // Timeout to fire up Alert
 setTimeout(() => {
-  new Alert();
+	require.ensure([], function (require) {
+		var Alert = require('./messaging').Alert;
+		new Alert();
+	});
 }, 5000);
